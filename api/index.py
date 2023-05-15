bbcaa0ffdc44fdce02730d011887d7cfccc9028f
Yash Singh <saiansh2525@gmail.com>
2023-05-14 17:02:48 -0700

    feat: flask cors

--- api/index.py
+++ api/index.py
@@ -3,8 +3,11 @@ import json
 import pinecone
 import openai
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 openai.api_key = os.getenv('OPENAI_KEY')
 pinecone.init(
     environment=os.getenv('PINECONE_ENVIRONMENT'),
