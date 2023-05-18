2cf40fe6815b3f4dbe8a666a3388db6aef008aa8
shreyvish5678 <129219936+shreyvish5678@users.noreply.github.com>
2023-05-17 18:10:34 -0700

    Update ChatGPT.py

--- ChatGPT.py
+++ ChatGPT.py
@@ -11,24 +11,28 @@ prompt = 'You are a helpful assistant that does the following: Your main job wil
   medical issues, problems or conditions based on user information provided. The user information will be in\
   a dictionary. This dictionary will consist of variables, such as age, sex, symptoms, past medical history, \
   current injury/trauma to the area, medications, and lifestyle factors. Your output will also be in the form\
-  of a dictionary consist of the top 5 medical issues, conditions, or problems the user could be having, and \
-  corresponding to\ them, you will provide another dictionary, which will consist of the probability that the \
-  user could be experiencing, listed with one of the five "Very High, High, Moderate, Low, Very Low". You will \
-  also provide a 20-30 word summary of each of the medical issues, problems or conditions, in that dictionary. \
+  of a dictionary consist of the top 5 medical issues, conditions, or problems the user could be having, and\
+  corresponding to\ them, you will provide another dictionary, which will consist of the probability that the\
+  user could be experiencing, listed with one of the five "Very High, High, Moderate, Low, Very Low". You will\
+  also provide a 20-30 word summary of each of the medical issues, problems or conditions, in that dictionary.\
   The only output you will return is the dictionary.'
-example_info = '{"age":15,"sex":"male","symptoms":"pain in the wrist, inability of motion near the wrist", \
+example_info = '{"age":15,"sex":"male","symptoms":"pain in the wrist, inability of motion near the wrist",\
   "recent injury/trauma to the area":"got hit with a soccer ball at a high speed, in the wrist","medications":\
-  "vitamin D3","past medical issues":"fractured same wrist two years ago, in an injury, took two months to \
+  "vitamin D3","past medical issues":"fractured same wrist two years ago, in an injury, took two months to\
   heal","lifestyle":"plays a lot of soccer, eats well"}'
-example_answer = '{"Wrist sprain/strain":{"probability":"Very High","summary":"Stretching or tearing of wrist \
-  ligaments due to sudden force or excessive bending, leading to pain and limited motion."},"Wrist fracture": \
-  {"probability":"Moderate","summary":"A break in the bones of the wrist, which may occur from trauma or re-injury, \
+example_answer = '{"Wrist sprain/strain":{"probability":"Very High","summary":"Stretching or tearing of wrist\
+  ligaments due to sudden force or excessive bending, leading to pain and limited motion."},"Wrist fracture":\
+  {"probability":"Moderate","summary":"A break in the bones of the wrist, which may occur from trauma or re-injury,\
   causing pain, swelling, and difficulty moving the wrist."},"Ligament Tear":{"probability":"Moderate","summary":\
-  "Damage to the ligaments in the wrist, often caused by trauma, leading to pain, instability, and compromised \
-  wrist function."},"Tendonitis":{"probability":"Low","summary":"Inflammation of the tendons in the wrist, \
+  "Damage to the ligaments in the wrist, often caused by trauma, leading to pain, instability, and compromised\
+  wrist function."},"Tendonitis":{"probability":"Low","summary":"Inflammation of the tendons in the wrist,\
   typically caused by repetitive or excessive use, resulting in pain and restricted movement."},"Arthritis":\
-  {"probability":"Very Low","summary":"Inflammation of the joints in the wrist, which can occur at a young age \
+  {"probability":"Very Low","summary":"Inflammation of the joints in the wrist, which can occur at a young age\
   as well, causing pain, stiffness, and limited range of motion."}}'
+print(prompt)
+print(example_info)
+print(example_answer)
+'''
 @app.route('/query')
 def query():
     user_info = request.args.get('user_info')
@@ -47,3 +51,4 @@ def query():
     return(str(response))
 if __name__ == "__main__":
     app.run()
+'''
