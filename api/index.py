51e28519f1d2ea9d800688ef96292f7a9a885616
shreyvish5678 <129219936+shreyvish5678@users.noreply.github.com>
2023-05-18 20:25:45 -0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,16 +2,17 @@ from flask import Flask, request
 import openai
 from flask_cors import CORS
 import os
+import json
+import pinecone
 app = Flask(__name__)
 CORS(app)
 openai.api_key = os.getenv('OPENAI_KEY')
-#Variables
 prompt = 'You are a helpful assistant that does the following: Your main job will be to provide the top 5\
   medical issues, problems or conditions based on user information provided. The user information will be in\
-  a dictionary. This dictionary will consist of variables, such as age, sex, symptoms, past medical history,\
+  a dictionary. This dictionary will consist of variables, such as age, sex, symptoms, past medical history, \
   current injury/trauma to the area, medications, and lifestyle factors. Your output will also be in the form\
   of a dictionary consist of the top 5 medical issues, conditions, or problems the user could be having, and\
-  corresponding to\ them, you will provide another dictionary, which will consist of the probability that the\
+  corresponding to them, you will provide another dictionary, which will consist of the probability that the\
   user could be experiencing, listed with one of the five "Very High, High, Moderate, Low, Very Low". You will\
   also provide a 20-30 word summary of each of the medical issues, problems or conditions, in that dictionary.\
   Finally, you will also provide what kind of doctor or treatment they should see, in 1-3 words in that\
@@ -20,20 +21,26 @@ prompt = 'You are a helpful assistant that does the following: Your main job wil
 example_info = '{"age":15,"sex":"male","symptoms":"pain in the wrist, inability of motion near the wrist",\
   "recent injury/trauma to the area":"got hit with a soccer ball at a high speed, in the wrist",\
   "past medical issues":"fractured same wrist two years ago, in an injury, took two months to\
-  heal","lifestyle &medications":"plays a lot of soccer, eats well, takes vitamin D3"}'
+  heal","lifestyle/medications":"plays a lot of soccer, eats well, takes vitamin D3"}'
 example_answer = '{"Wrist sprain/strain":{"probability":"Very High","summary":"Stretching or tearing of wrist\
   ligaments due to sudden force or excessive bending, leading to pain and limited motion.", "treatment": "Orthopedic\
-  Doctors"},"Wrist fracture": {"probability":"Moderate","summary":"A break in the bones of the wrist, which may\
-  occur from trauma or re-injury, causing pain, swelling, and difficulty moving the wrist.", "treatment": "Orthopedic Doctors"},"Ligament Tear":\
+  Doctor"},"Wrist fracture": {"probability":"Moderate","summary":"A break in the bones of the wrist, which may\
+  occur from trauma or re-injury, causing pain, swelling, and difficulty moving the wrist."},"Ligament Tear":\
   {"probability":"Moderate","summary":"Damage to the ligaments in the wrist, often caused by trauma, leading\
-  to pain, instability, and compromised wrist function.", "treatment": "Orthopedic Doctors"},"Tendonitis":\
+  to pain, instability, and compromised wrist function.", "treatment": "Orthopedic Doctor"},"Tendonitis":\
   {"probability":"Low","summary":"Inflammation of the tendons in the wrist, typically caused by repetitive or \
-  excessive use, resulting in pain and restricted movement.", "treatment": "Orthopedic Doctors"},"Arthritis":\
+  excessive use, resulting in pain and restricted movement.", "treatment": "Orthopedic Doctor"},"Arthritis":\
   {"probability":"Very Low","summary":"Inflammation of the joints in the wrist, which can occur at a young age\
   as well, causing pain, stiffness, and limited range of motion.", "treatment": "Rheumatologists"}}'
+pinecone.init(
+    environment=os.getenv('PINECONE_ENV'),
+    api_key=os.getenv("PINECONE_KEY")
+)
+index = pinecone.Index('dataset-of-yelp-embeddings')
 @app.route('/query')
 def query():
     user_info = request.args.get('user_info')
+    query_returns = []
     message = openai.ChatCompletion.create(
           model="gpt-3.5-turbo",
           messages=[
@@ -43,9 +50,21 @@ def query():
                 {"role": "user", "content": user_info}
           ]
       )
-    output = message.get('choices')[0]
-    content = output.get('message')
-    response = content.get('content')
-    return(response)
+    dictionary = json.loads(message['choices'][0]['message']['content'])
+    resources = []
+    for condition, info in dictionary.items():
+        treatment = info["treatment"]
+        resources.append(treatment)
+    for vector in resources:
+        treatment_vectors = openai.Embedding.create(input=vector, engine='text-embedding-ada-002')['data'][0]['embedding']
+        results = index.query(treatment_vectors, top_k=5, include_metadata=True, namespace='example-namespace')
+    for num in range(5):
+        match_string = results['matches'][num]['metadata']['metadata_key']
+        query_returns.append(match_string)
+    count = 0
+    for key, value in dictionary.items():
+        dictionary[key].update({"metadata": query_returns[count]})
+        count += 1
+    return json.dumps(dictionary)
 if __name__ == "__main__":
     app.run()
