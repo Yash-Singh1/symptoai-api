7418db81ef53c38d7497cfaca5c5e0b09fccfe7f
shreyvish5678 <129219936+shreyvish5678@users.noreply.github.com>
2023-05-28 14:47:57 -0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -41,6 +41,38 @@ example_answer = '{"Wrist sprain/strain":{"probability":"Very High","summary":"S
   excessive use, resulting in pain and restricted movement.", "treatment": "Orthopedic Doctor"},"Arthritis":\
   {"probability":"Very Low","summary":"Inflammation of the joints in the wrist, which can occur at a young age\
   as well, causing pain, stiffness, and limited range of motion.", "treatment": "Rheumatologists"}}'
+prompt2 = 'You are a helpful assistant that does the following: Your main job will be to provide the top 5\
+  medical issues, problems or conditions based on user information provided. The user information will be in\
+  a dictionary. This dictionary will consist of variables, such as age, sex, symptoms, past medical history, \
+  current injury/trauma to the area, medications, and lifestyle factors. Your output will also be in the form\
+  of a dictionary consist of the top 5 medical issues, conditions, or problems the user could be having, and\
+  corresponding to them, you will provide another dictionary, which will consist of the probability that the\
+  user could be experiencing, listed with one of the five "Very High, High, Moderate, Low, Very Low". You will\
+  also provide a 20-30 word summary of each of the medical issues, problems or conditions, in that dictionary.\
+  Finally, you will also provide what kind of doctor they should see, in 1-3 words in that\
+  dictionary. The specialist doctor can be the same across all issues but does not have to be.\
+  Along with the problems, and their respective probabilities and doctor specialists, you will also\
+  provide a list, which will have 5 elements, and each element will consist of a treatment or ailment\
+  they can do while at home, regardless of which medical problem they have. It should be an overall\
+  treatment. The only output you will return is the dictionary, which should adhere to the JSON spec.'
+example_answer2 = example_answer = '{"Problems": {"Wrist sprain/strain":{"probability":"Very High","summary":"Stretching or tearing of wrist\
+  ligaments due to sudden force or excessive bending, leading to pain and limited motion.", "treatment": "Orthopedic\
+  Doctor"},"Wrist fracture": {"probability":"Moderate","summary":"A break in the bones of the wrist, which may\
+  occur from trauma or re-injury, causing pain, swelling, and difficulty moving the wrist."},"Ligament Tear":\
+  {"probability":"Moderate","summary":"Damage to the ligaments in the wrist, often caused by trauma, leading\
+  to pain, instability, and compromised wrist function.", "treatment": "Orthopedic Doctor"},"Tendonitis":\
+  {"probability":"Low","summary":"Inflammation of the tendons in the wrist, typically caused by repetitive or \
+  excessive use, resulting in pain and restricted movement.", "treatment": "Orthopedic Doctor"},"Arthritis":\
+  {"probability":"Very Low","summary":"Inflammation of the joints in the wrist, which can occur at a young age\
+  as well, causing pain, stiffness, and limited range of motion.", "treatment": "Rheumatologists"}}, "Treatments": }\
+  ["Allow your wrist to rest and avoid activities that exacerbate the pain or strain the injured area. Consider\
+  using a splint or brace to immobilize the wrist and provide support.", "Apply ice packs or cold compresses to the\
+  affected wrist for 15-20 minutes every few hours. This can help reduce pain, inflammation, and swelling. Remember\
+  to always wrap the ice pack in a thin cloth to protect your skin.", "Wearing a compression bandage or wrap around\
+  your wrist can help reduce swelling and provide stability. Make sure not to wrap it too tightly, as it may restrict\
+  blood flow.", "Elevate your wrist above heart level whenever possible to reduce swelling. Prop your wrist up on\
+  pillows or use a sling to keep it elevated.", "Once the acute pain and swelling subside, you can start incorporating\
+  gentle exercises and stretches to promote flexibility and strengthen the wrist."]}'
 pinecone.init(
     environment=os.getenv('PINECONE_ENV'),
     api_key=os.getenv("PINECONE_KEY")
@@ -65,6 +97,45 @@ def query():
                 {"role": "system", "content": prompt},
                 {"role": "user", "content": example_info},
                 {"role": "assistant", "content": example_answer},
+                {"role": "user", "content": user_info}
+          ]
+      )
+    dictionary = json.loads(message['choices'][0]['message']['content'])
+    resources = []
+    queries = []
+    for _condition, info in dictionary.items():
+        treatment = info["treatment"]
+        resources.append(treatment)
+    for vector in resources:
+        treatment_vectors = openai.Embedding.create(input=vector, engine='text-embedding-ada-002')['data'][0]['embedding']
+        results = index.query(treatment_vectors, top_k=5, include_metadata=True, namespace='example-namespace')
+        queries.append(results)
+    for num in range(5):
+        match_string = queries[num]['matches'][0]['metadata']['metadata_key']
+        query_returns.append(match_string)
+    count = 0
+    for key, value in dictionary.items():
+        dictionary[key].update({"metadata": query_returns[count]})
+        count += 1
+    return json.dumps(dictionary)
+@app.route('/query2')
+def query2():
+    # We already have redis based verification
+    # if request.headers.get('Authorization') != os.getenv('API_KEY'):
+    #     return '{ "error": "Unauthenticated" }', 401
+    if request.headers.get('X-SymptoAI-Auth') is None:
+        return '{ "error": "Provide authentication header" }', 401
+    if r.get(request.headers.get('X-SymptoAI-Auth')) is None:
+        return '{ "error": "Authentication token not registered" }', 401
+    r.delete(request.headers.get('X-SymptoAI-Auth'))
+    user_info = request.args.get('user_info')
+    query_returns = []
+    message = openai.ChatCompletion.create(
+          model="gpt-3.5-turbo",
+          messages=[
+                {"role": "system", "content": prompt2},
+                {"role": "user", "content": example_info},
+                {"role": "assistant", "content": example_answer2},
                 {"role": "user", "content": user_info}
           ]
       )
